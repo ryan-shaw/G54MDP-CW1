5cb1829de0f3a8c56cffb3441acc7327d4a95ada
Ryan <ryan.shaw@min.vc>
2015-03-17 16:07:53 +0000

    Changed stuff.

--- app/src/main/java/vc/min/ryan/stopwatch/MainActivity.java
+++ app/src/main/java/vc/min/ryan/stopwatch/MainActivity.java
@@ -132,7 +132,8 @@ public class MainActivity extends Activity {
             TimerItem item = (TimerItem) intent.getParcelableExtra("obj");
             for(int i = 0; i < mAdapter.getData().size(); i++){
                 TimerItem item1 = mAdapter.getData().get(i);
-                if(item1.getId() == item.getId() && !item1.getFormattedTime().equals(item.getFormattedTime())){
+                Log.d("test", item.getFormattedTime() + ":"+item1.getFormattedTime());
+                if(item1.getId() == item.getId()){
                     item1 = item;
                     mAdapter.notifyItemChanged(i);
                 }
